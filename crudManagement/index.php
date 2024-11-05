083541934ec027e831a2ac1002b3f98cd5ae536f
Syaiful ipul <127600767+Ipul1122@users.noreply.github.com>
2024-11-06 01:41:17 +0700

    Update index.php

--- crudManagement/index.php
+++ crudManagement/index.php
@@ -175,30 +175,6 @@ $result = $conn->query("SELECT * FROM users");
     </div>
 </div>
 
-<script>
-    function editUser(user) {
-        $('#id').val(user.id);
-        $('#username').val(user.username);
-        $('#password').val(''); // Kosongkan password
-        $('#namaLengkap').val(user.namaLengkap);
-        $('#email').val(user.email);
-        $('#telepon').val(user.telepon);
-        if (user.banned) {
-            $('#bannedYes').prop('checked', true);
-        } else {
-            $('#bannedNo').prop('checked', true);
-        }
-        $('#aksesAdmin').prop('checked', user.akses & 1);
-        $('#aksesOperator').prop('checked', user.akses & 2);
-        $('#userModal').modal('show');
-    }
-
-    function resetForm() {
-        $('#userForm')[0].reset();
-        $('#id').val('');
-        $('#aksesAdmin').prop('checked', false);
-        $('#aksesOperator').prop('checked', false);
-    }
-</script>
+<script src="script.js"> </script>
 </body>
 </html>
